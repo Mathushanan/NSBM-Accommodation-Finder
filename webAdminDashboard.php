ba582e85e0e1c45e165b4224f505f7000dd74994
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-03-09 18:51:57 +0530

    WebAdminDashboard, StudentDashboard, WardenDashboard & LandlordDashboard pages created

---
+++ webAdminDashboard.php
@@ -0,0 +1,96 @@
+<?php
+
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UniNest NSBM</title>
+    <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css\all.min.css">
+</head>
+
+<body>
+
+
+
+    <nav>
+        <div class="nav__logo">UniNest NSBM</div>
+        <ul class="nav__links">
+            <li class="link"><a href="index.php">Home</a></li>
+            <li class="link"><a href="#footer_section">Contact</a></li>
+            <li class="link"><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
+
+
+
+
+
+    <div class="login-container">
+        <p>Webadmin</p>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="footer" id="footer_section">
+        <div class="section__container footer__container">
+            <div class="footer__col">
+                <h3>UniNest NSBM</h3>
+                <p>
+                    UniNest NSBM is Your one-stop platform for hassle-free student accommodation near NSBM Green University Town.
+                    With diverse housing options, finding your perfect place to stay has never been easier.
+
+                </p>
+                <p>
+                    Say goodbye to accommodation worries and hello to a stress-free booking experience with UniNest NSBM.
+                </p>
+            </div>
+            <div class="footer__col">
+                <h4>Company</h4>
+                <p>About Us</p>
+                <p>Our Team</p>
+                <p>Contact Us</p>
+            </div>
+            <div class="footer__col">
+                <h4>Legal</h4>
+                <p>FAQs</p>
+                <p>Terms & Conditions</p>
+                <p>Privacy Policy</p>
+            </div>
+            <div class="footer__col">
+                <h4>Resources</h4>
+                <ul class="social-icons">
+                    <li><a href="#" class="fab fa-facebook-f"></a></li>
+                    <li><a href="#" class="fab fa-twitter"></a></li>
+                    <li><a href="#" class="fab fa-instagram"></a></li>
+                    <li><a href="#" class="fab fa-linkedin-in"></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer__bar">
+            Copyright © nsbm. All rights reserved.
+        </div>
+
+    </footer>
+
+</body>
+
+</html>
